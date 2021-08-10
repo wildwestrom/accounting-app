9339c593858dfc90df3ada46a9553c2c39644afe
Christian Westrom <c.westrom@westrom.xyz>
2021-08-10 16:31:07 -0400

    frontend is done

--- test/wildwestrom/accounting_app_test.clj
+++
@@ -1,7 +0,0 @@
-(ns wildwestrom.accounting-app-test
-  (:require [clojure.test :refer :all]
-            [wildwestrom.accounting-app :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))

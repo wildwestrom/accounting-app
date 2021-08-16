e6076b3429e69c79651edf1f37e847db5ec44577
Christian Westrom <c.westrom@westrom.xyz>
2021-08-16 06:57:30 -0400

    Input fields have default values, added more test-data

--- src/app/frontend/db.cljs
+++ src/app/frontend/db.cljs
@@ -1,4 +1,38 @@
 (ns app.frontend.db)
 
 (def default-db
-  {:all-data [["Test Data Co. Ltd." "123599" "10/20/2021"]]})
+  {:all-data [{:payable-to "Test Data Co. Ltd."
+               :amount "12499"
+               :date "2021-01-01"}
+              {:payable-to "Test Data Co. Ltd."
+               :amount "12499"
+               :date "2021-02-01"}
+              {:payable-to "Test Data Co. Ltd."
+               :amount "12499"
+               :date "2021-03-01"}
+              {:payable-to "Test Data Co. Ltd."
+               :amount "12499"
+               :date "2021-04-01"}
+              {:payable-to "Test Data Co. Ltd."
+               :amount "12499"
+               :date "2021-05-01"}
+              {:payable-to "Test Data Co. Ltd."
+               :amount "12499"
+               :date "2021-06-01"}
+              {:payable-to "Test Data Co. Ltd."
+               :amount "12499"
+               :date "2021-07-01"}
+              {:payable-to "Test Data Co. Ltd."
+               :amount "12499"
+               :date "2021-08-01"}
+              {:payable-to "Test Data Co. Ltd."
+               :amount "12499"
+               :date "2021-08-05"}
+              {:payable-to "Test Data Co. Ltd."
+               :amount "199"
+               :date "2021-08-07"}
+              {:payable-to "Test Data Co. Ltd."
+               :amount "1099"
+               :date "2021-08-11"}
+              ]}
+  )
--- src/app/frontend/views.cljs
+++ src/app/frontend/views.cljs
@@ -1,27 +1,63 @@
 (ns app.frontend.views
   (:require [re-frame.core :as rf]
             [reagent-material-ui.components :as mui]
-            [app.frontend.subs :as subs]))
+            [reagent.core :as r]
+            [app.frontend.subs :as subs]
+            [clojure.string :as str]
+            [goog.string :as gstring]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Components
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defn leading-zero
+  [num]
+  (gstring/format "%02d" num))
+
+(defn current-date
+  []
+  (let [inst (js/Date.)]
+    (str (.getFullYear inst)
+         "-"
+         (leading-zero (.getMonth inst))
+         "-"
+         (leading-zero (.getDate inst)))))
+
+(defn current-time
+  []
+  (let [inst (js/Date.)]
+    (str (leading-zero (.getHours inst))
+         ":"
+         (leading-zero (.getMinutes inst)))))
+
 (defn input-fields []
   [mui/table-row
    [mui/table-cell
-    [mui/text-field {:type        "string"
-                     :required    true
-                     :placeholder "Payable to..."}]]
+    [mui/text-field {:type         "string"
+                     :required     true
+                     :placeholder  "Payable to..."
+                     :defaultValue "Company Name"
+                     :label        "Required"}]]
    [mui/table-cell
-    [mui/text-field {:type        "number"
-                     :required    true
-                     :step        1
-                     :placeholder "Amount..."}]]
+    [mui/text-field {:type         "number"
+                     :required     true
+                     :step         1
+                     :placeholder  "Amount..."
+                     :defaultValue "0"
+                     :label        "Required"
+                     :InputProps   {:end-adornment (r/as-element [mui/input-adornment {:position "end"} "₴"])}}]]
    [mui/table-cell
-    [mui/text-field {:type     "date"
-                     :required true}]
-    [mui/text-field {:type "time"}]]])
+    [mui/text-field {:type         "date"
+                     :required     true
+                     :defaultValue (current-date)
+                     :onChange     #(println (.. % -target -value))}]
+    [mui/text-field {:type         "time"
+                     :defaultValue (current-time)
+                     :onChange     #(println (.. % -target -value))}]]
+   [mui/table-cell
+    [mui/button {:variant "outlined"
+                 :type    "submit"}
+     "Submit"]]])
 
 (defn table-body
   []
@@ -29,8 +65,15 @@
     [mui/table-body
      (for [row @table]
        [mui/table-row {:key (random-uuid)}
-        (for [cell row]
-          [mui/table-cell {:key (random-uuid)} cell])])]))
+        (let [{:keys [payable-to amount date]} row]
+          [:<>
+           [mui/table-cell payable-to]
+           [mui/table-cell
+            [mui/grid {:container      true
+                       :justifyContent "space-between"}
+             [:span amount]
+             [:span "₴"]]]
+           [mui/table-cell date]])])]))
 
 (defn table
   []
